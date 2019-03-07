e5ae95c62fa90acd6dbd3e71f7750a827655cb35
anagutjor <anagjordano@gmail.com>
2019-03-07 18:08:26 +0100

    disallow duplicity in user name and email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
 const User = require('../schemas');
 const Joi = require('joi');
+const HttpStatus = require('http-status-codes');
 
 const UserValidator = Joi.object().keys({
     username: Joi.string().alphanum().min(3).max(30).required(),
     email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-    password: Joi.string().required()
+    password: Joi.string().min(4).required()
 })
 
 module.exports = {
@@ -13,11 +14,33 @@ module.exports = {
         console.log(user);
         Joi.validate( user, UserValidator, (err) => {
             if (!err) {
-                User.create({user}, (err) => {
-                    if (!err) {
-                        console.log('user succesfully registered!');
+                
+                //Check if an user with the same username or e-mail already exists
+                let username = user.username;
+                let email = user.email;
+
+                User.findOne({username}, (err, userFound) => {
+                    console.log(userFound)
+                    if ( userFound !== null ) {
+                        console.log('an user with the same username already exists!');
+                        return;
+                    } else {
+                        User.findOne({email}, (err, userFound) => {
+                            console.log(userFound)
+                            if ( userFound !== null ) {
+                                console.log('an user with the same e-mail already exists!');
+                                return;
+                            }
+                            User.create(user, (err) => {
+                                if (!err) {
+                                    console.log('user succesfully registered!');
+                                }
+                            }) 
+                        })
                     }
                 })
+            } else if (err && err.details) {
+                return res.status(HttpStatus.BAD_REQUEST).json({ message: err.details });
             } else {
                 console.error(err);
             }
