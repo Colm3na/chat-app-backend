44a93bc114548019677abf4e2cf2dd50fa2fb005
anagutjor <anagjordano@gmail.com>
2019-03-07 14:02:21 +0100

    add joi validation. returns 'ValidationError'

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,25 @@
 const User = require('../schemas');
+const Joi = require('joi');
+
+const UserValidator = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+    password: Joi.string().regex(/[.*]{3,30}$/).required()
+})
 
 module.exports = {
     createUser(req, res) {
         let user = req.body;
         console.log(user);
-        User.create({user}, (err) => {
+        Joi.validate( {user}, UserValidator, (err) => {
             if (!err) {
-                console.log('user succesfully registered!');
+                User.create({user}, (err) => {
+                    if (!err) {
+                        console.log('user succesfully registered!');
+                    }
+                })
+            } else {
+                console.error(err);
             }
         })
     }
