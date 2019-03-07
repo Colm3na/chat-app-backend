a5b853b5da4f5d07d57db090f5c9f2eca03d5eee
anagutjor <anagjordano@gmail.com>
2019-03-07 12:23:56 +0100

    install cookie-parser

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cookieSecret = process.env.COOKIE_SECRET;
+
 const app = express();
 
+app.use(cookieParser(cookieSecret));
 app.use(express.static(__dirname + 'public'))
+
+app.get('/', function (req, res) {
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies)
+  })
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
