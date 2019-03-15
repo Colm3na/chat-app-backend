c937412406213cc296462db074803aae33301613
anagutjor <anagjordano@gmail.com>
2019-03-15 11:50:06 +0100

    add first event

--- socket/streams.js
+++ socket/streams.js
@@ -1,5 +1,12 @@
 module.exports = function(io) {
     io.on('connection', (socket) => {
         console.log('User connected');
+
+        socket.on('button', () => {
+            console.log('even sent')
+            socket.emit('event', {
+                message: 'button has been pressed'
+            })
+        })
     })
 }
