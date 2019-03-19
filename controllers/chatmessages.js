0ab7c707c72bedd4905543e584d5d36f1b632e89
anagutjor <anagjordano@gmail.com>
2019-03-19 12:36:45 +0100

    add joi validation to message

--- controllers/chatmessages.js
+++ controllers/chatmessages.js
@@ -1,19 +1,38 @@
 const Models = require('../schemas');
 const Message = Models.Message;
 const HttpStatus = require('http-status-codes');
+const Joi = require('joi');
+
+const MessageValidator = Joi.object().keys({
+    body: Joi.string().required(),
+    senderId: Joi.any().required(),
+    receiverId: Joi.any(),
+    sender: Joi.string().required(),
+    receiver: Joi.string(),
+    isRead: Joi.bool(),
+    createdAt:  Joi.date()
+})
 
 module.exports = {
     async saveMessage(req, res) {
         let message = req.body;
-        await Message.create(message)
-            .then( () => {
-                res
-                    .status(HttpStatus.CREATED)
-                    .json({message: 'Chat message succesfully saved', message})
-            })
-            .catch( err => {
-                console.error(err);
-            })
+        await Joi.validate(message, MessageValidator, (err) => {
+            if (!err) {
+                Message.create(message)
+                .then( () => {
+                    res
+                        .status(HttpStatus.CREATED)
+                        .json({message: 'Chat message succesfully saved', message})
+                })
+                .catch( err => {
+                    console.error(err);
+                })
+            } else {
+                return res
+                    .status(HttpStatus.BAD_REQUEST)
+                    .json({ message: err.details });
+            }
+        }) 
     },
 
     async getMessage(req, res) {
