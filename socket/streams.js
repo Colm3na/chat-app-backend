c3852b03dc0109c6314f00aa07eb3bbc105bc792
anagutjor <anagjordano@gmail.com>
2019-03-20 13:03:34 +0100

    add socket online to find online users

--- socket/streams.js
+++ socket/streams.js
@@ -1,9 +1,20 @@
-module.exports = function(io) {
+module.exports = function(io, _) {
+    let globalArray = [];
     io.on('connection', (socket) => {
         console.log('User connected');
 
         socket.on('new message', (data) => {
             console.log(data);
         })
+
+        socket.on('online', data => {
+            socket.join(data.room);
+            const user = { id: socket.id, name: data.user, room: data.room };
+            globalArray.push(user);
+            const roomName = globalArray.filter(user => user.room === data.room);
+            const list = roomName.map(user => user.name);
+            console.log(_.uniq(list))
+            io.emit('usersOnline', _.uniq(list));
+        });
     })
 }
