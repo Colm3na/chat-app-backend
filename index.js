acfa0709963c4b2236ce1f8904394a08cea55119
anagutjor <anagjordano@gmail.com>
2019-03-22 12:27:52 +0100

    add jwt token as socket authentication method

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const jwt = require('jsonwebtoken');
+const dbConfig = require('./config/secrets');
 const _ = require('lodash');
 
 const auth = require('./routes/authRoutes');
@@ -15,8 +17,6 @@ const chatmessages = require('./routes/chatmsgRoutes');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser(cookieSecret));
-
-require('./socket/streams')(io);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -28,6 +28,20 @@ app.use(express.static(__dirname + 'public'))
 app.use('/api/chatapp', users);
 app.use('/api/chatapp', auth);
 app.use('/api/chatapp', chatmessages);
+
+io.use((socket, next) => {
+  const token = socket.handshake.query.token;
+ 
+  // verify token
+  jwt.verify(token, dbConfig.secret, (err, decoded) => {
+    if(err) return next(err);
+    // set the user’s mongodb _id to the socket for future use
+    socket._id = decoded.data._id;
+    next();
+  });
+});
+
+require('./socket/streams')(io);
 
 http.listen(3000, () => {
     console.log('Server listening on port 3000');
