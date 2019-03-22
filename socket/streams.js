09dd63e232e34cee2075ae565bdff6d91fb128b5
anagutjor <anagjordano@gmail.com>
2019-03-22 12:27:01 +0100

    simplify format

--- socket/streams.js
+++ socket/streams.js
@@ -3,27 +3,28 @@ module.exports = function(io) {
     io.on('connection', (socket) => {
         console.log('User connected');
 
-        socket.on('new message', (data) => {
-            console.log(data);
-        })
+        socket
+            .on('new message', (data) => {
+                console.log(data);
+            })
 
-        socket.on('online', data => {
-            console.log(data)
-            socket.join(data.room);
-            const user = { id: socket.id, name: data.user, room: data.room, _id: data.userId };
-            globalArray.push(user);
-            const roomName = globalArray.filter(user => user.room === data.room);
-            const list = roomName.map( function(user) { return [user.name, user._id] });
-            // remove duplicates
-            const uniqueList = list.map(JSON.stringify).reverse().filter(function (e, i, a) {
-                return a.indexOf(e, i+1) === -1;
-            }).reverse().map(JSON.parse) 
+            .on('online', data => {
+                console.log(data)
+                socket.join(data.room);
+                const user = { id: socket.id, name: data.user, room: data.room, _id: data.userId };
+                globalArray.push(user);
+                const roomName = globalArray.filter(user => user.room === data.room);
+                const list = roomName.map( function(user) { return [user.name, user._id] });
+                // remove duplicates
+                const uniqueList = list.map(JSON.stringify).reverse().filter(function (e, i, a) {
+                    return a.indexOf(e, i+1) === -1;
+                }).reverse().map(JSON.parse) 
 
-            io.emit('usersOnline', uniqueList);
-        });
+                io.emit('usersOnline', uniqueList);
+            })
 
-        socket.on('disconnect', () => {
-            console.log('disconnect fired');
-        })
+            .on('disconnect', () => {
+                console.log('disconnect fired');
+            })
     })
 }
